6f266399c987d032937867c65e1d31b5cc1a1d69
myusername <himashawije5678@gmail.com>
2024-05-20 23:32:33 +0530

    react-default logo removed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './pages/home';
+import "./App.css";
+import Home from "./pages/home";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
-
+      <Home />
     </div>
   );
 }
