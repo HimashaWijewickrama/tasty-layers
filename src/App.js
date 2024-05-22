8acd90e944c428e89d5836dd4f44a396d356399b
myusername <himashawije5678@gmail.com>
2024-05-22 19:22:29 +0530

    deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Home />
-      <AboutusView/>
+      <AboutusView />
       <ProductContainer />
       <ReviewContainer />
       <ContactusContainer />
